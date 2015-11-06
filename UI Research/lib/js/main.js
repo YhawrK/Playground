c9b9a66b2b5ea17ef6b0dfa4342478809cfbb64d
James Edward <jamesedwarddev@gmail.com>
2015-11-06 12:50:19 -0600

    found better slider

--- UI Research/lib/js/main.js
+++ UI Research/lib/js/main.js
@@ -1,19 +1,12 @@
-$(document).ready(function() {
-// leave the first line and closing brackets
+$( document ).ready(function() {
 
 
 
-
-
-
-
-$("#slider").slider();
-
-
-
-
-
-
+$("#ex13").slider({
+    ticks: [0, 2, 4, 6, 8, 10, 12, 14, 16],
+    ticks_labels: ['0', '2', '4', '6', '8', '10', '12', '14', '16'],
+    ticks_snap_bounds: .5
+});
 
 
 
